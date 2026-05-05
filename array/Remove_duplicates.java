e214a30077db71e3bd35863371094466b2b876d6
Vaishnavi-KN16 <iyervaishnavi60@gmail.com>
2026-05-05 22:05:52 +0530

    arrays

---
+++ array/Remove_duplicates.java
@@ -0,0 +1,14 @@
+public class Remove_duplicates{
+    public int remove(int[] nums)
+    [
+        int k =0;
+        for( int i=0;i<nums.length;i++)
+        {
+            for( int j=1;j<nums.length;j++)
+
+            {
+                
+            }
+        }
+    ]
+}
